00dd90ac70b9bf908b05d8ce701c855f6fda1333
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2025-04-09 18:19:27 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="min-w-screen min-h-screen flex py-4 px-16">
+            <div className={"w-2/5 bg-blue-500 min-h-full flex flex-col justify-center items-center"}>
+                <div className={"w-24 h-36 bg-red-300"}></div>
+                <div className={"text-center flex flex-col items-center justify-center gap-y-4 mt-4 w-2/3"}>
+                    <h1 className={"text-2xl font-bold"}>Create your own Poster!</h1>
+                    <p>It's so simple. Start createing your own poster by clicking one of the action buttons locaten on
+                        the right.</p>
+                    <p className={"font-light"}>(Ratio 4:5)</p>
+                </div>
+
+            </div>
+            <div className={"w-3/5 bg-red-500 min-h-full"}>B</div>
+        </div>
+    );
 }
 
 export default App;
